3c314546eef35a4ff83af7ca708092719043713b
Telkamp, Philip <philip.telkamp@mines.sdsmt.edu>
2018-12-19 14:59:16 -0700

    Initial Commit, contains basic python bounding box script

---
+++ boundingBox/openCam.py
@@ -0,0 +1,31 @@
+from cb import drawBox
+import cb
+import cv2
+import numpy as np
+
+# Begins reading from the default webcam
+cap = cv2.VideoCapture(0)
+
+ret, frame = cap.read()
+
+cv2.namedWindow('Feed')
+cv2.setMouseCallback('Feed', drawBox)
+
+while(1):
+	
+	# Reads the next frame from the camera
+	ret, frame = cap.read()
+
+	if ret == False:
+		break
+
+	cv2.rectangle(frame, (cb.ix, cb.iy), (cb.fx, cb.fy), (255, 204, 51), 1)
+
+	cv2.imshow('Feed', frame)
+
+    # Use 'Q' to exit program
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+cv2.destroyAllWindows()
+cap.release()
